588d953ca7645c11808f08f85c40fc1d2cf3308e
Shoxrux Yuldashov <deagleshox@mail.ru>
2025-01-22 12:09:37 +0300

    API Docs
    - Добавлена документация API
    - Подключены эндпоинты `users/` и `api/`
    - Документация в двух вариантах:
        - Swagger `baseURL/swagger/`
        - Redoc `baseURL/redoc/`

--- vps_manager/settings.py
+++ vps_manager/settings.py
@@ -1,4 +1,6 @@
+from datetime import timedelta
 from pathlib import Path
+from typing import Any
 
 from decouple import Csv, config
 from django.core.management.utils import get_random_secret_key
@@ -19,12 +21,20 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # установленные пакеты
+    "rest_framework",
+    "rest_framework_simplejwt",
+    "drf_yasg",
+    "corsheaders",
+    # приложения проекта
     "vps.apps.VpsConfig",
+    "users.apps.UsersConfig",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -92,7 +102,30 @@ MEDIA_ROOT = BASE_DIR / "media/"
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "static/"
 
-CORS_ALLOWED_ORIGINS = config("CORS_ALLOWED_ORIGINS", cast=Csv())
-CSRF_TRUSTED_ORIGINS = config("CSRF_TRUSTED_ORIGINS", cast=Csv())
+CORS_ALLOWED_ORIGINS: list[str] = config("CORS_ALLOWED_ORIGINS", cast=Csv())
+CSRF_TRUSTED_ORIGINS: list[str] = config("CSRF_TRUSTED_ORIGINS", cast=Csv())
+CORS_ALLOW_ALL_ORIGINS: bool = config("CORS_ALLOW_ALL_ORIGINS", cast=bool)
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+AUTH_USER_MODEL = "users.User"
+
+PAGE_SIZE: int = 25
+MAX_PAGE_SIZE: int = 100
+PAGE_SIZE_QUERY_PARAM: str = "page_size"
+
+REST_FRAMEWORK: dict[str, Any] = {
+    "DEFAULT_FILTER_BACKENDS": (
+        "django_filters.rest_framework.DjangoFilterBackend",
+    ),
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+    "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.AllowAny",),
+    "DEFAULT_PAGINATION_CLASS": "api.pagination.DefaultPagination",
+    "PAGE_SIZE": PAGE_SIZE,
+}
+
+SIMPLE_JWT: dict[str, Any] = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+}
